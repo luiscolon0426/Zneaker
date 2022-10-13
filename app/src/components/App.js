ac1a66328feb443c06035c394e6be6790196ce63
Eiden <eidnegabriel07@gmail.com>
2022-10-13 18:06:44 -0400

    adding routing

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,12 +1,23 @@
 import Banner from './Banner';
 import Navbar from './Navbar';
-
+import Apy from './Apy';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Banner/>
-    </div>
+    <Router>
+      <div className="App">
+          <Switch>
+            <Route path="/" exact>
+              <Navbar/>
+              <Banner/>
+            </Route>
+
+            <Route path="/app" exact>
+              <Apy/>
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ app/src/components/Apy.js
@@ -0,0 +1,7 @@
+const Apy = () => {
+  return ( 
+    <h1>Lechugis</h1>
+   );
+}
+ 
+export default Apy;
