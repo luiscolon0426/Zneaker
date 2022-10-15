5224a4b2b4df4a2c4e2269566199e5c74153b215
Eiden <eidnegabriel07@gmail.com>
2022-10-15 10:35:29 -0400

    changes to navbar and adding new component

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,18 +1,20 @@
 import '../styles/Navbar.css';
-import { logIn } from '../backend/auth'
+import { logIn } from '../backend/auth';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return ( 
     <nav className="navbar">
       <h1>_codePocket</h1>
       <div className="links">
-        <a href="/">About</a>
-        <a href="/">Features</a>
-        <a href="/">Team</a>
+        <Link to="/">About</Link>
+        <Link to="/">Features</Link>
+        <Link to="/">Team</Link>
       </div>
       <div className="login">
-          <a href="/">Log in</a>
-          <button onClick={logIn}>Get started</button>
+          {/*<Link to="/" onClick={logIn}>Log in</Link>*/}
+          <button id='getIn' onClick={logIn}>Log in</button>
+          <button>Sign up</button>
         </div>
     </nav>
    );
