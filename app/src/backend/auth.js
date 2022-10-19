a0eece3daef9fb26fa9334b740ab13eae408dd6f
David <4292@holbertonschool.com>
2022-10-19 12:20:31 -0400

    fixed some semistandard

--- app/src/backend/auth.js
+++ app/src/backend/auth.js
@@ -1,54 +1,59 @@
 // HANDLES AUTHENTICATION //
 
-import { auth } from "../firebase.js"
-import { GithubAuthProvider, signInWithRedirect, onAuthStateChanged, getRedirectResult, signOut } from 'firebase/auth'
-import { addUser, saveToken, deleteToken } from "./db.js"
+import { auth } from '../firebase.js';
+import { GithubAuthProvider, signInWithRedirect, onAuthStateChanged, getRedirectResult, signOut } from 'firebase/auth';
+import { addUser, saveToken, deleteToken } from './db.js';
 
-const provider = new GithubAuthProvider()
+const provider = new GithubAuthProvider();
 
 // handles log in with Github
-export const logIn = function logIn() {
-    signInWithRedirect(auth, provider).catch((err) => {
-      console.log("Sign in error occurred");
-      console.log(err);
-    })
+export const logIn = function logIn () {
+  signInWithRedirect(auth, provider).catch((err) => {
+    console.log('Sign in error occurred');
+    console.log(err);
+  });
 };
 
 // handles log out
-export const logOut = async function logOut() {
-  await deleteToken()
+export const logOut = async function logOut () {
+  await deleteToken();
   // window.location.href = '/home'
   signOut(auth).then(() => {}).catch((err) => {
-    console.log("Sign out error occurred")
-    console.log(err)
-  })
-}
+    console.log('Sign out error occurred');
+    console.log(err);
+  });
+};
 
 export let uid;
 // Verifies if a user is logged in or not
 onAuthStateChanged(auth, (user) => {
   if (user) {
-  uid = user.uid
-  console.log("User logged in")
-  addUser()
+    uid = user.uid;
+    console.log('User logged in');
+    addUser();
   // if (window.location.href.indexOf('/home') > -1) {
   //   window.location.href = '/app'
   // }
   } else {
-    uid = 'False'
+    uid = 'False';
     if (window.location.href.indexOf('/app') > -1) {
-      window.location.href = '/'
+      window.location.href = '/';
     }
   }
 });
 
 // Gets redirect results of log in
 getRedirectResult(auth).then(res => {
-  try{
-    const credential = GithubAuthProvider.credentialFromResult(res)
-    const token = credential.accessToken
-    saveToken(token)
+  try {
+    const credential = GithubAuthProvider.credentialFromResult(res);
+    const token = credential.accessToken;
+    saveToken(token);
   } catch (e) {
-    console.log("Error caught")
+    if (e instanceof TypeError) {
+      console.log('No redirection');
+    } else {
+      console.log('Redirection error ocurred');
+      console.log(e);
+    }
   }
-})
+});
