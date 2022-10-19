da4235ab7e7841680f904ecbb3c369dde1b0054c
David <4292@holbertonschool.com>
2022-10-19 12:19:29 -0400

    Added display file functionality

--- app/src/backend/api.js
+++ app/src/backend/api.js
@@ -1,96 +1,116 @@
-import axios from "axios";
-import { getToken, userInfo } from "./db";
+import axios from 'axios';
+import { getToken, userInfo } from './db';
 
-let data = '';
-
+const data = '';
 
 // Gets users github info (username)
-export const getGithubInfo = async function getGithubInfo() {
-    const token = await getToken()
-    let userName
-    let config = {
-        method: 'get',
-        url: 'https://api.github.com/user',
-        headers: { 
-          'Accept': 'application/json', 
-          'Authorization': `Bearer ${token}`
-        },
-        data : data
-      };
-      
-      return axios(config)
-      .then(function (response) {
-        userName = response.data.login;
-        return userName
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-}
+export const getGithubInfo = async function getGithubInfo () {
+  const token = await getToken();
+  let userName;
+  const config = {
+    method: 'get',
+    url: 'https://api.github.com/user',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    data: data
+  };
+
+  return axios(config)
+    .then(function (response) {
+      userName = response.data.login;
+      return userName;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 // Gets a list of valid repos
-export const getRepos = async function getRepos() {
-    let user;
-    await userInfo().then(res => {
-      user = res
-    })
-    let config = {
+export const getRepos = async function getRepos () {
+  let user;
+  await userInfo().then(res => {
+    user = res;
+  });
+  const config = {
     method: 'get',
     url: `https://api.github.com/users/${user.userName}/repos`,
-    headers: { 
-      'Accept': 'application/json', 
-      'Authorization': `Bearer ${user.authToken}`
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${user.authToken}`
     },
-    data : data
+    data: data
   };
 
   return axios(config)
-  .then(function (response) {
-    let repoList = []
-    response.data.forEach(repo => {
-      repoList.push(repo.name)
+    .then(function (response) {
+      const repoList = [];
+      response.data.forEach(repo => {
+        repoList.push(repo.name);
+      });
+      const validRepo = repoList.filter(repo => { return repo.includes('.codepocket'); });
+      return validRepo;
     })
-    let validRepo = repoList.filter(repo => { return repo.includes('.codepocket') })
-    return validRepo;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 // Gets a list of all files inside valid repo
-export const getFiles = async function getFiles() {
-  let user
+export const getFiles = async function getFiles (repo) {
+  let user;
   await userInfo().then(res => {
-    user = res
-  })
-  let config = {
-    method: 'get',
-    url: `https://api.github.com/repos/${user.userName}/test.codepocket/contents/`,
-    headers: { 
-      'Accept': 'application/json', 
-      'Authorization': `Bearer ${user.authToken}` 
-    },
-    data : data
-  };
-  
-  return axios(config)
-  .then(function (response) {
-    let fileList = []
-    response.data.forEach(file => {
-      fileList.push(file.path)
-    })
-    let validFiles = fileList.filter(file => { return file.includes('.js') })
-    return validFiles;
-  })
-  .catch(function (error) {
-    console.log(error);
+    user = res;
   });
-  
-}
+  const config = {
+    method: 'get',
+    url: `https://api.github.com/repos/${user.userName}/${repo}/contents/`,
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${user.authToken}`
+    },
+    data: data
+  };
 
+  return axios(config)
+    .then(function (response) {
+      const fileList = [];
+      response.data.forEach(file => {
+        fileList.push(file.path);
+      });
+      const validFiles = fileList.filter(file => { return file.includes('.js'); });
+      return validFiles;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
+// Gets a single specified file from Github
+export const getSingleFile = async function getSingleFile (repo, filename) {
+  let user;
+  await userInfo().then(res => {
+    user = res;
+  });
+  const config = {
+    method: 'get',
+    url: `https://api.github.com/repos/${user.userName}/${repo}/contents/${filename}`,
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${user.authToken}`
+    },
+    data: data
+  };
 
+  return axios(config)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 // export const printRepoList = async function printRepoList() {
 //   const repoList = await getFiles()
--- app/src/backend/display.js
+++ app/src/backend/display.js
@@ -1,35 +1,51 @@
-import { doc } from "firebase/firestore";
-import { getRepos, getFiles } from "./api";
+import { getRepos, getFiles, getSingleFile } from './api';
 
-let firstClick = false
-export const fillThePocket = async function fillThePocket() {
-    if (firstClick === false) {
-        firstClick = true
-        const repos = await getRepos()
-        const files =  await getFiles(repos[0])
+let firstClick = false;
+export const fillThePocket = async function fillThePocket () {
+  if (firstClick === false) {
+    firstClick = true;
+    const repos = await getRepos();
+    const files = await getFiles(repos[0]);
 
-        // let fileObj = {}
-        // for (let i = 0; i < repos.length; i++) {
-        //     let files = await getFiles(repos[i])
-        //     fileObj[repos[i]] = files
-        // }
-        // let repo_file_List = [repos, fileObj]
-    
-        console.log("Filling with the pocket...")
-        let repoTag = document.getElementById('repo')
-        let fileTag = document.getElementById('file')
-    
-        repoTag.innerHTML = `${repos[0]}`
+    // let fileObj = {}
+    // for (let i = 0; i < repos.length; i++) {
+    //     let files = await getFiles(repos[i])
+    //     fileObj[repos[i]] = files
+    // }
+    // let repo_file_List = [repos, fileObj]
 
-        for (let i = 0; i < files.length; i++) {
-            let opt = document.createElement('option')
-            opt.text = files[i]
-            opt.addEventListener("click", () => {
-                console.log(opt.text)
-            })
+    console.log('Filling with the pocket...');
+    const repoTag = document.getElementById('repo');
+    const fileTag = document.getElementById('file');
+    const fboxTag = document.getElementById('fileBox');
 
-            fileTag.appendChild(opt)
-        }
-        // repoTag.appendChild(opt1)
+    repoTag.innerHTML = `${repos[0]}`;
+
+    for (let i = 0; i < files.length; i++) {
+      const opt = document.createElement('option');
+      opt.text = files[i];
+      opt.addEventListener('click', () => {
+        // console.log(opt.text)
+        displayFile(repos[0], files[i]).then(res => [
+          fboxTag.innerHTML = res
+        ]);
+      });
+
+      fileTag.appendChild(opt);
     }
-}
+    // repoTag.appendChild(opt1)
+  }
+};
+
+export const displayFile = async function displayFile (repo, fileName) {
+  let file;
+  await getSingleFile(repo, fileName).then(res => {
+    file = res;
+  });
+  const data = file.content;
+  console.log(data);
+  const buff = atob(data);
+  const text = buff.toString('ascii');
+
+  return text;
+};
