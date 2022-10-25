ee8f3f21b740b3c2d60d1202adfbc39c40e7bd98
Eiden <eidnegabriel07@gmail.com>
2022-10-25 16:09:29 -0400

    added app page home button

--- app/src/components/Altnav.js
+++ app/src/components/Altnav.js
@@ -4,7 +4,9 @@ import { logOut } from '../backend/auth';
 const Navbar = () => {
   return ( 
     <nav className="nav_bar">
-      <h1>_codePocket</h1>
+      <h1 onClick={()=>{
+        window.location.href='/'
+      }}>_codePocket</h1>
           {/*<Link to="/" onClick={logIn}>Log in</Link>*/}
           <div className='logoutContainer'>
             <button onClick={logOut}>Log out</button>
